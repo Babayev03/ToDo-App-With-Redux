dcacd486879274ac2ef62051054363d41229e6d0
Babayev03 <ilhambabayev222@gmail.com>
2023-05-24 12:15:44 -0700

    Updated code

--- src/screens/ToDoList.tsx
+++ src/screens/ToDoList.tsx
@@ -2,7 +2,6 @@ import { View, Text, TouchableOpacity, ScrollView, FlatList } from 'react-native
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../App';
-import { Dimensions } from 'react-native';
 import { Checkbox } from 'react-native-paper';
 
 const ToDoList = () => {
