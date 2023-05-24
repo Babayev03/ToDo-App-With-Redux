7ee674e2caebcfe3376c199aea8284d4c19ab5ba
Babayev03 <ilhambabayev222@gmail.com>
2023-05-24 12:14:14 -0700

    Updated Code

--- src/screens/AddToDo.tsx
+++ src/screens/AddToDo.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -6,65 +7,72 @@ import {
   TouchableOpacity,
   Alert,
 } from 'react-native';
-import React, {useState} from 'react';
-import {useDispatch} from 'react-redux';
-import { Checkbox } from 'react-native-paper';
+import { useDispatch } from 'react-redux';
 
 const AddTodo = () => {
-  const [title, settitle] = useState('');
-  const [checked, setChecked] = React.useState(false);
-
-  let dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
 
   const addToDo = () => {
     if (title === '') {
       Alert.alert('Empty ToDo', 'Please Enter ToDo then press Add again', [
-        {text: 'OK'},
+        { text: 'OK' },
       ]);
       return;
     }
+
     let newTodoItem = {
       id: Math.floor(Math.random() * 10000),
       title: title,
       completed: false,
     };
 
-    dispatch({type: 'ADD_TODO', payload: newTodoItem});
-    settitle('');
+    dispatch({ type: 'ADD_TODO', payload: newTodoItem });
+    setTitle('');
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
         style={styles.input}
-        onChangeText={settitle}
+        onChangeText={setTitle}
         placeholderTextColor={'purple'}
-        placeholder="Enter To Do"
+        placeholder="Enter ToDo"
         value={title}
       />
       <TouchableOpacity
         onPress={addToDo}
-        style={{
-          backgroundColor: 'purple',
-          marginHorizontal: 80,
-          alignItems: 'center',
-          paddingVertical: 10,
-          borderRadius: 10,
-        }}>
-        <Text style={{fontSize: 15, color: '#fff'}}>Add To Do</Text>
+        style={styles.addButton}
+      >
+        <Text style={styles.buttonText}>Add ToDo</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
-export default AddTodo;
-
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 8,
+  },
   input: {
     height: 40,
-    margin: 12,
+    marginVertical: 8,
     borderWidth: 1,
     padding: 10,
     color: '#000',
   },
+  addButton: {
+    backgroundColor: 'purple',
+    alignItems: 'center',
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 15,
+    color: '#fff',
+  },
 });
+
+export default AddTodo;
